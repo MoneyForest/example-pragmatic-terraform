49b2824609f3019c6b73b00dd364e0382154252a
MoneyForest <kanamorishuhei@gmail.com>
2021-02-22 15:39:24 +0900

    fix: alb

---
+++ alb_http.tf
@@ -0,0 +1,79 @@
+# ALBはAWSが提供するLB
+
+# HTTPアクセス可能なALB
+# load_balancer_typeで種別を設定する
+# internalでインターネット向けかVPC内部向けか指定
+# idle_timeoutでタイムアウト値を設定
+# enable_deletion_protectionで削除保護
+# subnetは異なるAZのサブネットを指定してクロスゾーン負荷分散をする
+# access_logsにバケット名を指定するとアクセスログの保存が有効になる
+
+resource "aws_lb" "example" {
+  name                       = "example"
+  load_balancer_type         = "application"
+  internal                   = false
+  idle_timeout               = 60
+  enable_deletion_protection = false
+
+  subnets = [
+    aws_subnet.public_0.id,
+    aws_subnet.public_1.id
+  ]
+
+  access_logs {
+    bucket  = aws_s3_bucket.alb_log.id
+    enabled = true
+  }
+
+  security_groups = [
+    module.http_sg.security_group_id,
+    module.https_sg.security_group_id,
+    module.http_redirect_sg.security_group_id,
+  ]
+}
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+}
+
+module "http_sg" {
+  source      = "./security_group"
+  name        = "http-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "https_sg" {
+  source      = "./security_group"
+  name        = "https-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 443
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "http_redirect_sg" {
+  source      = "./security_group"
+  name        = "https-redirect-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 8080
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+# リスナー
+# default_action　いずれのルールにも合致しない場合実行
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "これは「HTTP」です"
+      status_code  = "200"
+    }
+  }
+}

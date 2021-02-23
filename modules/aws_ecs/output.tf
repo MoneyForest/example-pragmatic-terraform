1cc2467204033ff9a6e63ed770f54f72e31ff90f
MoneyForest <kanamorishuhei@gmail.com>
2021-02-23 19:47:09 +0900

    refactor

---
+++ modules/aws_ecs/output.tf
@@ -0,0 +1,3 @@
+output "iam_role_arn" {
+    value = module.ecs_task_execution_role.iam_role_arn
+}
--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "aws_lb_example_dns_name" {
 output "aws_route53_record_example_name" {
   value = module.aws_route53_record.example_name
 }
+
+output "iam_role_arn" {
+    value = module.aws_ecs.iam_role_arn
+}
